96e60e1e8e5a116431ba2145eb4e25904e3c5504
ATOMPEAR <ATOMPEAR@OUTLOOK.COM>
2024-10-16 23:02:37 -0600

    MAIN COMMIT
    -- REMOVED ICON FROM TITLEBAR AND RELATED CODE

--- resources/source/java/preload.js
+++ resources/source/java/preload.js
@@ -4,6 +4,5 @@ contextBridge.exposeInMainWorld('electronAPI', {
   ping: () => ipcRenderer.invoke('ping'),
   minimizeWindow: () => ipcRenderer.invoke('minimize-window'),
   maximizeWindow: () => ipcRenderer.invoke('maximize-window'),
-  closeWindow: () => ipcRenderer.invoke('close-window'),
-  onSetTitlebarIcon: (callback) => ipcRenderer.on('set-titlebar-icon', callback)
+  closeWindow: () => ipcRenderer.invoke('close-window')
 })
--- resources/source/java/renderer.js
+++ resources/source/java/renderer.js
@@ -12,10 +12,4 @@ document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('close-button').addEventListener('click', () => {
     window.electronAPI.closeWindow()
   })
-
-  // Set titlebar icon
-  window.electronAPI.onSetTitlebarIcon((event, iconDataUrl) => {
-    const titlebarIcon = document.getElementById('titlebar-icon')
-    titlebarIcon.style.backgroundImage = `url(${iconDataUrl})`
-  })
 })
